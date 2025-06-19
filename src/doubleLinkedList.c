fc8d51c4cb2d2a09e97aff3f34c07acc8bb9234f
Shenal Herath <smh231@uclive.ac.nz>
2025-06-19 14:18:22 +1200

    created functions for inserting and deleteing nodes

--- src/doubleLinkedList.c
+++ src/doubleLinkedList.c
@@ -1,1 +1,49 @@
 #include "doubleLinkedList.h"
+#include <stdlib.h>
+
+/**
+ * @brief Adds newNode after the specified node in the doubly linked list
+ * @param list
+ * @param node
+ * @param newNode
+ */
+void InsertAfterNode(DoublyLinkedList *list, DoublyLinkedNode *node, DoublyLinkedNode *newNode)
+{
+}
+
+/**
+ * @brief Adds newNode before the specified node in the doubly linked list
+ * @param list
+ * @param node
+ * @param newNode
+ */
+void InsertBeforeNode(DoublyLinkedList *list, DoublyLinkedNode *node, DoublyLinkedNode *newNode)
+{
+}
+
+/**
+ * @brief Adds newNode to the head of the doubly linked list
+ * @param list
+ * @param newNode
+ */
+void InsertAtHead(DoublyLinkedList *list, DoublyLinkedNode *newNode)
+{
+}
+
+/**
+ * @brief Adds newNode to the tail of the doubly linked list
+ * @param list
+ * @param newNode
+ */
+void InsertAtTail(DoublyLinkedList *list, DoublyLinkedNode *newNode)
+{
+}
+
+/**
+ * @brief Remote the specified node from the doubly linked list and free its memory
+ * @param list
+ * @param node
+ */
+void DeleteNode(DoublyLinkedList *list, DoublyLinkedNode *node)
+{
+}
--- src/doubleLinkedList.h
+++ src/doubleLinkedList.h
@@ -1,15 +1,21 @@
 #ifndef DOUBLE_LINKED_LIST_H
 #define DOUBLE_LINKED_LIST_H
 
-typedef struct DoublyLinkedNode{
+typedef struct {
     int data;
     struct DoublyLinkedNode* next;
     struct DoublyLinkedNode* prev;
-};
+} DoublyLinkedNode;
 
-typedef struct DoublyLinkedList{
+typedef struct {
     struct DoublyLinkedNode* head;
     struct DoublyLinkedNode* tail;
-};
+} DoublyLinkedList;
+
+void InsertAfterNode(DoublyLinkedList* list, DoublyLinkedNode* node, DoublyLinkedNode* newNode);
+void InsertBeforeNode(DoublyLinkedList* list, DoublyLinkedNode* node, DoublyLinkedNode* newNode);
+void InsertAtHead(DoublyLinkedList* list, DoublyLinkedNode* newNode);
+void InsertAtTail(DoublyLinkedList* list, DoublyLinkedNode* newNode);
+void DeleteNode(DoublyLinkedList* list, DoublyLinkedNode* node);
 
 #endif
