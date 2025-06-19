3dfc2dce8ff5ffdd26e07edbe91666392e3ba3f0
Shenal Herath <smh231@uclive.ac.nz>
2025-06-20 00:55:22 +1200

    updated main

--- main.c
+++ main.c
@@ -5,5 +5,16 @@
 #include "src/doubleLinkedList.h"
 
 int main(void){
+    DoublyLinkedList* list = calloc(1, sizeof(DoublyLinkedList));
+    DoublyLinkedNode* node1 = calloc(1, sizeof(DoublyLinkedNode));
+    DoublyLinkedNode* node2 = calloc(1, sizeof(DoublyLinkedNode));
+    DoublyLinkedNode* node3 = calloc(1, sizeof(DoublyLinkedNode));
+
     printf("Hello, World!\n");
+
+    free(list);
+    free(node1);
+    free(node2);
+    free(node3);
+    return 0;
 }
